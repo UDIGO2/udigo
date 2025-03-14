73c8b699362ecef7fcd5d0bc0f5a0c8e953fdfa3
hyun1203 <gdo654@gmail.com>
2025-03-14 17:23:54 +0900

    <기능>(회원가입) : 회원가입 css 추가 & 로그인 실패페이지 수정중

--- src/main/java/com/udigo/hotel/config/SecurityConfig.java
+++ src/main/java/com/udigo/hotel/config/SecurityConfig.java
@@ -17,33 +17,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/cart","/payment","/payList", "/css/**", "/js/**", "/image/**","/reservations/**","/acm/**","/board/**").permitAll() // 메인 페이지와 정적 리소스 접근 허용
-                        // 결제페이지 실제 진행 시, 로그인 후에 되도록 없애기
-                        .requestMatchers("/member/signup", "/member/api/**").permitAll() // 회원가입 관련 모든 요청 허용
-                        .anyRequest().authenticated() // 나머지는 로그인 필요
-                )
-                .formLogin(login -> login
-                        .loginPage("/login") // 로그인 페이지 지정
-                        .defaultSuccessUrl("/", true) // 로그인 성공 시 메인 페이지로 이동
-                        .permitAll()
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout") // 로그아웃 URL 지정
-                        .logoutSuccessUrl("/") // 로그아웃 후 메인 페이지 이동
-                        .permitAll()
-                )
-                .csrf(csrf -> csrf.disable());  // CSRF 보호 비활성화 (개발 중)
-
-
-        return http.build();
-
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
